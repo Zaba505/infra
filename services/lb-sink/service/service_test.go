24eabd7d4cc2002e22ee68d1baf60b6b2561a9a9
Richard Carson Derr <cakub6@gmx.com>
2023-12-15 18:40:43 -0500

    story(refactor): use z5labs/app http runtime instead of custom implementation (#40)
    * refactor(issue-38): migrate to z5labs app http runtime
    
    * refactor(issue-38): leverage otel and slog helper packages
    
    * refactor(issue-38): update based on new func signature
    
    * refactor(issue-38): update imports from z5labs/app to z5labs/bedrock
    
    * deps(issue-38): update bazel to 7.0.0

--- services/lb-sink/service/service_test.go
+++ services/lb-sink/service/service_test.go
@@ -8,147 +8,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestStartupHandler(t *testing.T) {
-	t.Run("will return 200 response code", func(t *testing.T) {
-		t.Run("if started flag is true", func(t *testing.T) {
-			rt := &runtime{}
-			rt.started.Store(true)
-
-			w := httptest.NewRecorder()
-			r := httptest.NewRequest(http.MethodGet, "http://example.com/healthy/startup", nil)
-
-			rt.startupHandler(w, r)
-
-			if !assert.Equal(t, http.StatusOK, w.Result().StatusCode) {
-				return
-			}
-		})
-	})
-
-	t.Run("will return 503 response code", func(t *testing.T) {
-		t.Run("if started flag is false", func(t *testing.T) {
-			rt := &runtime{}
-			rt.started.Store(false)
-
-			w := httptest.NewRecorder()
-			r := httptest.NewRequest(http.MethodGet, "http://example.com/healthy/startup", nil)
-
-			rt.startupHandler(w, r)
-
-			if !assert.Equal(t, http.StatusServiceUnavailable, w.Result().StatusCode) {
-				return
-			}
-		})
-
-		t.Run("if started flag is zero", func(t *testing.T) {
-			rt := &runtime{}
-
-			w := httptest.NewRecorder()
-			r := httptest.NewRequest(http.MethodGet, "http://example.com/healthy/startup", nil)
-
-			rt.startupHandler(w, r)
-
-			if !assert.Equal(t, http.StatusServiceUnavailable, w.Result().StatusCode) {
-				return
-			}
-		})
-	})
-}
-
-func TestLivenessHandler(t *testing.T) {
-	t.Run("will return 200 response code", func(t *testing.T) {
-		t.Run("if healthy flag is true", func(t *testing.T) {
-			rt := &runtime{}
-			rt.healthy.Store(true)
-
-			w := httptest.NewRecorder()
-			r := httptest.NewRequest(http.MethodGet, "http://example.com/healthy/liveness", nil)
-
-			rt.livenessHandler(w, r)
-
-			if !assert.Equal(t, http.StatusOK, w.Result().StatusCode) {
-				return
-			}
-		})
-	})
-
-	t.Run("will return 503 response code", func(t *testing.T) {
-		t.Run("if healthy flag is false", func(t *testing.T) {
-			rt := &runtime{}
-			rt.healthy.Store(false)
-
-			w := httptest.NewRecorder()
-			r := httptest.NewRequest(http.MethodGet, "http://example.com/healthy/liveness", nil)
-
-			rt.livenessHandler(w, r)
-
-			if !assert.Equal(t, http.StatusServiceUnavailable, w.Result().StatusCode) {
-				return
-			}
-		})
-
-		t.Run("if healthy flag is zero", func(t *testing.T) {
-			rt := &runtime{}
-
-			w := httptest.NewRecorder()
-			r := httptest.NewRequest(http.MethodGet, "http://example.com/healthy/liveness", nil)
-
-			rt.livenessHandler(w, r)
-
-			if !assert.Equal(t, http.StatusServiceUnavailable, w.Result().StatusCode) {
-				return
-			}
-		})
-	})
-}
-
-func TestReadinessHandler(t *testing.T) {
-	t.Run("will return 200 response code", func(t *testing.T) {
-		t.Run("if healthy flag is true", func(t *testing.T) {
-			rt := &runtime{}
-			rt.serving.Store(true)
-
-			w := httptest.NewRecorder()
-			r := httptest.NewRequest(http.MethodGet, "http://example.com/healthy/readiness", nil)
-
-			rt.readinessHandler(w, r)
-
-			if !assert.Equal(t, http.StatusOK, w.Result().StatusCode) {
-				return
-			}
-		})
-	})
-
-	t.Run("will return 503 response code", func(t *testing.T) {
-		t.Run("if healthy flag is false", func(t *testing.T) {
-			rt := &runtime{}
-			rt.serving.Store(false)
-
-			w := httptest.NewRecorder()
-			r := httptest.NewRequest(http.MethodGet, "http://example.com/healthy/readiness", nil)
-
-			rt.readinessHandler(w, r)
-
-			if !assert.Equal(t, http.StatusServiceUnavailable, w.Result().StatusCode) {
-				return
-			}
-		})
-
-		t.Run("if healthy flag is zero", func(t *testing.T) {
-			rt := &runtime{}
-
-			w := httptest.NewRecorder()
-			r := httptest.NewRequest(http.MethodGet, "http://example.com/healthy/readiness", nil)
-
-			rt.readinessHandler(w, r)
-
-			if !assert.Equal(t, http.StatusServiceUnavailable, w.Result().StatusCode) {
-				return
-			}
-		})
-	})
-}
-
 func TestUnavailableHandler(t *testing.T) {
 	testCases := []struct {
 		Name string
@@ -163,12 +22,12 @@ func TestUnavailableHandler(t *testing.T) {
 	for _, testCase := range testCases {
 		testCase := testCase
 		t.Run(testCase.Name, func(t *testing.T) {
-			rt := &runtime{}
+			h := unavailableHandler{}
 
 			w := httptest.NewRecorder()
 			r := httptest.NewRequest(http.MethodGet, "http://example.com"+testCase.Path, nil)
 
-			rt.unavailableHandler(w, r)
+			h.ServeHTTP(w, r)
 
 			if !assert.Equal(t, http.StatusServiceUnavailable, w.Result().StatusCode) {
 				return
--- services/machinemgmt/service/service.go
+++ services/machinemgmt/service/service.go
@@ -6,26 +6,31 @@ import (
 	"encoding/base64"
 	"fmt"
 	"io"
-	"net"
+	"log/slog"
 	"net/http"
-	"strconv"
-	"sync/atomic"
+	"os"
 
-	"github.com/Zaba505/infra/pkg/httpvalidate"
 	"github.com/Zaba505/infra/services/machinemgmt/service/backend"
 
 	"cloud.google.com/go/storage"
-	"github.com/uptrace/opentelemetry-go-extra/otelzap"
-	"github.com/z5labs/app"
-	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"github.com/z5labs/bedrock"
+	brhttp "github.com/z5labs/bedrock/http"
+	"github.com/z5labs/bedrock/http/httpvalidate"
+	"github.com/z5labs/bedrock/pkg/otelslog"
+	"github.com/z5labs/bedrock/pkg/slogfield"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/trace"
-	"go.uber.org/zap"
-	"golang.org/x/sync/errgroup"
 )
 
 type config struct {
+	OTel struct {
+		GCP struct {
+			ProjectId   string `config:"projectId"`
+			ServiceName string `config:"serviceName"`
+		} `config:"gcp"`
+	} `config:"otel"`
+
 	Http struct {
 		Port uint `config:"port"`
 	} `config:"http"`
@@ -39,179 +44,56 @@ type storageClient interface {
 	GetBootstrapImage(context.Context, *backend.GetBootstrapImageRequest) (*backend.GetBootstrapImageResponse, error)
 }
 
-type runtime struct {
-	log    *otelzap.Logger
-	listen func(string, string) (net.Listener, error)
-
-	// http
-	port uint
-
-	started atomic.Bool
-	healthy atomic.Bool
-	serving atomic.Bool
-
-	storage storageClient
-}
-
-func BuildRuntime(bc app.BuildContext) (app.Runtime, error) {
+func BuildRuntime(bc bedrock.BuildContext) (bedrock.Runtime, error) {
 	var cfg config
 	err := bc.Config.Unmarshal(&cfg)
 	if err != nil {
 		return nil, err
 	}
 
-	logger, err := zap.NewProduction()
-	if err != nil {
-		return nil, err
-	}
-
-	tp, err := configureOtel()
-	if err != nil {
-		logger.Error("failed to configure otel", zap.Error(err))
-		return nil, err
-	}
-	otel.SetTracerProvider(tp)
+	logger := slog.New(slog.NewJSONHandler(
+		os.Stderr,
+		&slog.HandlerOptions{
+			AddSource: true,
+		},
+	))
 
 	gs, err := storage.NewClient(context.Background())
 	if err != nil {
-		logger.Error("failed to create storage client", zap.Error(err))
+		logger.Error("failed to create storage client", slog.Any("error", err))
 		return nil, err
 	}
 	bucket := gs.Bucket(cfg.Storage.Bucket)
 	storageService := backend.NewStorageService(
-		backend.Logger(logger),
+		backend.Logger(logger.Handler()),
 		backend.GoogleCloudBucket(bucket),
 		backend.ObjectHasher(sha256.New),
 	)
 
-	rt := &runtime{
-		log:     otelzap.New(logger),
-		listen:  net.Listen,
-		port:    cfg.Http.Port,
-		storage: storageService,
-	}
+	rt := brhttp.NewRuntime(
+		brhttp.ListenOnPort(cfg.Http.Port),
+		brhttp.LogHandler(logger.Handler()),
+		brhttp.Handle(
+			"/bootstrap/image",
+			httpvalidate.Request(
+				http.Handler(&bootstrapImageHandler{
+					log:     otelslog.New(logger.Handler()),
+					storage: storageService,
+				}),
+				httpvalidate.ForMethods(http.MethodGet),
+				httpvalidate.ExactParams("id"),
+			),
+		),
+	)
 	return rt, nil
 }
 
-func (rt *runtime) Run(ctx context.Context) error {
-	conn, err := rt.listen("tcp", ":"+strconv.FormatUint(uint64(rt.port), 10))
-	if err != nil {
-		return err
-	}
-
-	mux := http.NewServeMux()
-	registerEndpoint(
-		mux,
-		"/health/startup",
-		httpvalidate.Request(
-			http.HandlerFunc(rt.startupHandler),
-			httpvalidate.ForMethods(http.MethodGet),
-		),
-	)
-	registerEndpoint(
-		mux,
-		"/health/liveness",
-		httpvalidate.Request(
-			http.HandlerFunc(rt.livenessHandler),
-			httpvalidate.ForMethods(http.MethodGet),
-		),
-	)
-	registerEndpoint(
-		mux,
-		"/health/readiness",
-		httpvalidate.Request(
-			http.HandlerFunc(rt.readinessHandler),
-			httpvalidate.ForMethods(http.MethodGet),
-		),
-	)
-	registerEndpoint(
-		mux,
-		"/bootstrap/image",
-		httpvalidate.Request(
-			http.HandlerFunc(rt.bootstrapImageHandler),
-			httpvalidate.ForMethods(http.MethodGet),
-			httpvalidate.ExactParams("id"),
-		),
-	)
-
-	s := &http.Server{
-		Handler: otelhttp.NewHandler(mux, "machinemgmt", otelhttp.WithMessageEvents(otelhttp.ReadEvents, otelhttp.WriteEvents)),
-	}
-
-	g, gctx := errgroup.WithContext(ctx)
-	g.Go(func() error {
-		<-gctx.Done()
-		defer func() {
-			tp := otel.GetTracerProvider()
-			stp, ok := tp.(interface {
-				Shutdown(context.Context) error
-			})
-			if !ok {
-				return
-			}
-			stp.Shutdown(context.Background())
-		}()
-
-		ctx, cancel := context.WithCancel(context.Background())
-		defer cancel()
-		defer rt.log.Info("stopped service")
-
-		s.Shutdown(ctx)
-		return nil
-	})
-	g.Go(func() error {
-		rt.log.Info("started serving")
-		rt.started.Store(true)
-		rt.healthy.Store(true)
-		rt.serving.Store(true)
-		return s.Serve(conn)
-	})
-
-	err = g.Wait()
-	if err == http.ErrServerClosed {
-		return nil
-	}
-	return err
+type bootstrapImageHandler struct {
+	log     *slog.Logger
+	storage storageClient
 }
 
-func registerEndpoint(mux *http.ServeMux, path string, h http.Handler) {
-	mux.Handle(
-		path,
-		otelhttp.WithRouteTag(path, h),
-	)
-}
-
-// report whether this service is ready to begin accepting traffic
-func (rt *runtime) startupHandler(w http.ResponseWriter, req *http.Request) {
-	started := rt.started.Load()
-	if started {
-		w.WriteHeader(http.StatusOK)
-		return
-	}
-	w.WriteHeader(http.StatusServiceUnavailable)
-}
-
-// report whether this service is healthy or needs to be restarted
-func (rt *runtime) livenessHandler(w http.ResponseWriter, req *http.Request) {
-	healthy := rt.healthy.Load()
-	if healthy {
-		w.WriteHeader(http.StatusOK)
-		return
-	}
-	w.WriteHeader(http.StatusServiceUnavailable)
-}
-
-// report whether this service is able to accept traffic
-func (rt *runtime) readinessHandler(w http.ResponseWriter, req *http.Request) {
-	serving := rt.serving.Load()
-	if serving {
-		w.WriteHeader(http.StatusOK)
-		return
-	}
-	w.WriteHeader(http.StatusServiceUnavailable)
-}
-
-func (rt *runtime) bootstrapImageHandler(w http.ResponseWriter, req *http.Request) {
+func (h *bootstrapImageHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	params := req.URL.Query()
 	imageId := params.Get("id")
 
@@ -220,12 +102,17 @@ func (rt *runtime) bootstrapImageHandler(w http.ResponseWriter, req *http.Reques
 	))
 	defer span.End()
 
-	resp, err := rt.storage.GetBootstrapImage(spanCtx, &backend.GetBootstrapImageRequest{
+	resp, err := h.storage.GetBootstrapImage(spanCtx, &backend.GetBootstrapImageRequest{
 		ID: imageId,
 	})
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		rt.log.Ctx(spanCtx).Error("failed to get bootstrap image", zap.String("image_id", imageId), zap.Error(err))
+		h.log.ErrorContext(
+			spanCtx,
+			"failed to get bootstrap image",
+			slogfield.String("image_id", imageId),
+			slogfield.Error(err),
+		)
 		return
 	}
 	defer resp.Body.Close()
@@ -237,7 +124,12 @@ func (rt *runtime) bootstrapImageHandler(w http.ResponseWriter, req *http.Reques
 	_, err = io.Copy(w, resp.Body)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		rt.log.Ctx(spanCtx).Error("failed to write image to response", zap.String("image_id", imageId), zap.Error(err))
+		h.log.ErrorContext(
+			spanCtx,
+			"failed to write image to response",
+			slogfield.String("image_id", imageId),
+			slogfield.Error(err),
+		)
 		return
 	}
 }
