cbb52d31d5ee1fa5ccc8d94633f366a6439e485c
Richard Carson Derr <cakub6@gmx.com>
2024-04-21 17:34:29 -0400

    refactor(issue-112): migrate sa module

--- service-account/google-cloud/main.tf
+++ service-account/google-cloud/main.tf
@@ -2,9 +2,13 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 5.6.0"
+      version = "5.25.0"
     }
   }
+}
+
+provider "google" {
+  project = var.project_id
 }
 
 locals {
