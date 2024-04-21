7538465558bf3a9a17fc2cc9778ddf98e50e0ebe
Richard Carson Derr <cakub6@gmx.com>
2024-04-21 17:35:27 -0400

    feat(issue-112): make a module for creating storage buckets

---
+++ storage-bucket/google-cloud/variables.tf
@@ -0,0 +1,16 @@
+variable "project_id" {
+  type = string
+}
+
+variable "name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "force_destroy" {
+  type = bool
+  default = true
+}
