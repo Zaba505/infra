f29d58dc6552baf17b83057e5bf5bcdcd55e8e17
Richard Carson Derr <cakub6@gmx.com>
2024-08-08 23:04:12 -0400

    story(ip): refactor to use regional ip addresses (#190)
    * refactor(issue-187): rename module
    
    * refactor(issue-187): use regional based address now

--- ip/google-external-regional-ipv6/main.tf
+++ ip/google-external-regional-ipv6/main.tf
@@ -7,8 +7,13 @@ terraform {
   }
 }
 
-resource "google_compute_global_address" "ipv6" {
+resource "google_compute_address" "ipv6" {
   name         = var.name
   ip_version   = "IPV6"
   address_type = "EXTERNAL"
+
+  # As of now, PREMIUM must be set since regional
+  # external IPv6 addresses are not supported for
+  # the STANDARD tier.
+  network_tier = "PREMIUM"
 }
