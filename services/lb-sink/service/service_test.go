aff464f81f44b49c5e8cecd825aa2e8b0733beec
Richard Carson Derr <cakub6@gmx.com>
2024-08-23 01:51:52 -0400

    story(issue-267): update to bedrock v0.9.0 (#273)
    * deps(issue-267): updated to latest bedrock version

--- services/lb-sink/service/service_test.go
+++ services/lb-sink/service/service_test.go
@@ -1,37 +1,9 @@
 package service
 
 import (
-	"net/http"
-	"net/http/httptest"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestUnavailableHandler(t *testing.T) {
-	testCases := []struct {
-		Name string
-		Path string
-	}{
-		{Name: "root", Path: "/"},
-		{Name: "sub", Path: "/hello"},
-		{Name: "deep", Path: "/hello/world"},
-		{Name: "params", Path: "/example?hello=world"},
-	}
 
-	for _, testCase := range testCases {
-		testCase := testCase
-		t.Run(testCase.Name, func(t *testing.T) {
-			h := unavailableHandler{}
-
-			w := httptest.NewRecorder()
-			r := httptest.NewRequest(http.MethodGet, "http://example.com"+testCase.Path, nil)
-
-			h.ServeHTTP(w, r)
-
-			if !assert.Equal(t, http.StatusServiceUnavailable, w.Result().StatusCode) {
-				return
-			}
-		})
-	}
 }
--- services/machinemgmt/main.go
+++ services/machinemgmt/main.go
@@ -4,16 +4,13 @@ import (
 	"bytes"
 	_ "embed"
 
-	"github.com/Zaba505/infra/pkg/framework"
-	"github.com/Zaba505/infra/services/machinemgmt/service"
+	"github.com/Zaba505/infra/pkg/rest"
+	"github.com/Zaba505/infra/services/machinemgmt/app"
 )
 
 //go:embed config.yaml
 var cfgSrc []byte
 
 func main() {
-	framework.RunHttp(
-		bytes.NewReader(cfgSrc),
-		service.Init,
-	)
+	rest.Run(bytes.NewReader(cfgSrc), app.Init)
 }
