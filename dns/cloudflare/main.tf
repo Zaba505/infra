baa90bf8a2431fbcc66c7c6674120488491687aa
Richard Carson Derr <cakub6@gmx.com>
2024-11-10 22:56:43 -0500

    story(issue-424): update dns cloudflare module (#425)

--- dns/cloudflare/main.tf
+++ dns/cloudflare/main.tf
@@ -8,61 +8,64 @@ terraform {
 }
 
 locals {
-  a_records = { for name, record in var.records : name => record.ipv4 if record.ipv4 != null }
+  a_records = merge([
+    for name, record in var.records : {
+      for ipv4 in record.ipv4 : "${name}-${ipv4.address}" => {
+        name    = name
+        address = ipv4.address
+      }
+    }
+    if record.ipv4 != null
+  ]...)
 
-  aaaa_records = { for name, record in var.records : name => record.ipv6 if record.ipv6 != null }
-
-  secured_records = { for name, record in var.records : name => record.certificate if record.certificate != null }
-
-  mtls_records = [for name, record in var.records : name if record.enable_mtls]
+  aaaa_records = merge([
+    for name, record in var.records : {
+      for ipv6 in record.ipv6 : "${name}-${ipv6.address}" => {
+        name    = name
+        address = ipv6.address
+      }
+    }
+    if record.ipv6 != null
+  ]...)
 }
 
 data "cloudflare_zone" "default" {
   name = var.domain_name
 }
 
-resource "cloudflare_record" "ipv4" {
+resource "cloudflare_record" "a" {
   for_each = local.a_records
 
   zone_id = data.cloudflare_zone.default.id
-  name    = each.key
+  name    = each.value.name
   value   = each.value.address
   type    = "A"
   proxied = true
 }
 
-resource "cloudflare_record" "ipv6" {
+resource "cloudflare_record" "aaaa" {
   for_each = local.aaaa_records
 
   zone_id = data.cloudflare_zone.default.id
-  name    = each.key
+  name    = each.value.name
   value   = each.value.address
   type    = "AAAA"
   proxied = true
 }
 
-resource "cloudflare_authenticated_origin_pulls_certificate" "per_hostname" {
-  depends_on = [
-    cloudflare_record.ipv4,
-    cloudflare_record.ipv6
-  ]
-  for_each = nonsensitive(toset(keys(local.secured_records)))
-
-  zone_id = data.cloudflare_zone.default.id
-  type    = "per-hostname"
-
-  certificate = local.secured_records[each.key].pem
-  private_key = local.secured_records[each.key].private_key
+locals {
+  mtls_proxy_records = toset([for name, record in var.records : name if record.authenticated_origin_pulls_enabled])
 }
 
 resource "cloudflare_authenticated_origin_pulls" "per_hostname" {
   depends_on = [
-    cloudflare_record.ipv4,
-    cloudflare_record.ipv6
+    cloudflare_record.a,
+    cloudflare_record.aaaa
   ]
-  for_each = toset(local.mtls_records)
+
+  for_each = local.mtls_proxy_records
 
   zone_id  = data.cloudflare_zone.default.id
   enabled  = true
-  hostname = "${each.key}.${var.domain_name}"
+  hostname = "${each.value}.${var.domain_name}"
 }
